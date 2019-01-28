9e2a417a2b12cb0d3cf46461d97ab45bf9f10ea9
Will Wray <will.wray@interdax.com>
2019-01-28 18:01:57 -0500

    main: verbose output, more examples

--- main.cpp
+++ main.cpp
@@ -2,9 +2,42 @@
 #include "type_name_rt.hpp"
 #include "type_name_pt.hpp"
 
+// std::array<char> output operator, for type_name_pt output
+template <size_t N>
+std::ostream& operator<<(std::ostream& o, std::array<char,N> const& a)
+{
+    for (char c : a) o.put(c);
+    return o;
+}
+
+constexpr char c{};
+
 int main()
 {
+    std::cout << "AUTO_NAME &c     :  " << auto_name_pt<&c> << '\n';
+    std::cout << "AUTO_NAME char{} :  " << auto_name_pt<char{}> << '\n';
+
+    std::cout << '\n';
+
+    std::cout << "TYPE_NAME char        _PT :  " << type_name_pt<char> << '\n';
+    std::cout << "                      _RT :  " << type_name_rt<char> << '\n';
+    std::cout << "TYPE_NAME int&        _PT :  " << type_name_pt<int&> << '\n';
+    std::cout << "                      _RT :  " << type_name_rt<int&> << '\n';
+    std::cout << "TYPE_NAME bool&&      _PT :  " << type_name_pt<bool&&> << '\n';
+    std::cout << "                      _RT :  " << type_name_rt<bool&&> << '\n';
+    std::cout << "TYPE_NAME int const&  _PT :  " << type_name_pt<int const&> << '\n';
+    std::cout << "                      _RT :  " << type_name_rt<int const&> << '\n';
+
+    std::cout << '\n';
+
     const volatile char abc[1][2][3]{};
-    std::cout << type_name_rt<decltype(abc)> << '\n';
-    std::cout << type_name_pt<decltype(abc)>.data() << std::endl;
+    using ABC = decltype(abc);
+
+    std::cout << "TYPE_NAME const volatile char[1][2][3]  _PT:  " << type_name_pt<ABC> << '\n';
+    std::cout << "                                        _RT:  " << type_name_rt<ABC> << '\n';
+
+    std::cout << '\n';
+
+    std::cout << "TYPE_NAME std::string :  _PT " << type_name_pt<std::string> << '\n';
+    std::cout << "                      :  _RT " << type_name_rt<std::string> << '\n';
 }
